09775dfd7b7cd28846521c037c2eb14e141734eb
Tanjim <tanjim.ak49@gmail.com>
2021-03-08 23:11:18 +0600

    all done except main func

--- Compiler (CSE 310)/Assignment1/SymbolTable.cpp
+++ Compiler (CSE 310)/Assignment1/SymbolTable.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int NUM_OF_BUCKETS;
 
 int hash_(string key, int numOfBuckets) {
     int sumOfAsciiValues = 0;
@@ -30,15 +32,22 @@ public:
 class ScopeTable {
     int numOfBuckets;
     SymbolInfo** buckets;
-    ScopeTable* parentScope;
+    ScopeTable* parent;
+    int level;
+    string id;
 
 public:
-    ScopeTable(int numOfBuckets) {
+    ScopeTable(int numOfBuckets, ScopeTable* parent, int level, int id) {
         this->numOfBuckets = numOfBuckets;
         buckets = new SymbolInfo*[numOfBuckets];
         for(int i=0; i<numOfBuckets; i++)
             buckets[i] = NULL;
-        parentScope = NULL;
+        this->parent = parent;
+        this->level = level;
+        if(parent != NULL)
+            this->id = parent->getId() + "." + to_string(id);
+        else
+            this->id = to_string(id);
     }
 
     bool insert(string name, string type) {
@@ -71,23 +80,95 @@ public:
             return false;
         if(current->getName() == name) {
             buckets[bucketNo] = current->getNext();
-            delete current;
             return true;
         }
         while(current->getNext() != NULL) {
             if(current->getNext()->getName() == name) {
                 current->setNext(current->getNext()->getNext());
-                delete current->getNext();
                 return true;
             }
             current = current->getNext();
         }
         return false;
     }
+
+    void print() {
+        cout << "ScopeTable # " + id << endl;
+        for(int i=0; i<numOfBuckets; i++) {
+            cout << i << " --> ";
+            SymbolInfo* current = buckets[i];
+            while(current != NULL) {
+                cout << "<" << current->getName() << ", " << current->getType() << "> ";
+                current = current->getNext();
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    ScopeTable* getParent() {return parent;}
+    int getLevel() {return level;}
+    string getId() {return id;}
+
+    ~ScopeTable() {
+        delete[] buckets;
+    }
 };
 
 class SymbolTable {
+    ScopeTable* currentScopeTable;
+    vector<int> idCounts; // to track scope table ids of levels
+public:
+    SymbolTable() {
+        idCounts = {1}; //level 1 corresponds to index 0
+        currentScopeTable = new ScopeTable(NUM_OF_BUCKETS, NULL, 1, 1);
+    }
 
+    void enterScope() {
+        int newLevel = currentScopeTable->getLevel() + 1;
+        if(newLevel > idCounts.size())
+            idCounts.push_back(1);
+        else
+            idCounts[newLevel - 1] ++;
+        ScopeTable* newScopeTable = new ScopeTable(NUM_OF_BUCKETS, currentScopeTable, newLevel, idCounts[newLevel - 1]);
+        currentScopeTable = newScopeTable;
+    }
+
+    void exitScope() {
+        ScopeTable* temp = currentScopeTable->getParent();
+        delete currentScopeTable;
+        currentScopeTable = temp;
+    }
+
+    bool insert(string name, string type) {
+        return currentScopeTable->insert(name, type);
+    }
+
+    bool delete_(string name) {
+        return currentScopeTable->delete_(name);
+    }
+
+    SymbolInfo* lookUp(string name) {
+        ScopeTable* current = currentScopeTable;
+        while(current != NULL) {
+            SymbolInfo* symbolInfo = current->lookUp(name);
+            if(symbolInfo != NULL)
+                return symbolInfo;
+            current = current->getParent();
+        }
+        return NULL;
+    }
+
+    void printCurrentScopeTable() {
+        currentScopeTable->print();
+    }
+
+    void printAllScopeTables() {
+        ScopeTable* current = currentScopeTable;
+        while(current != NULL) {
+            current->print();
+        }
+    }
 };
 
 int main() {
