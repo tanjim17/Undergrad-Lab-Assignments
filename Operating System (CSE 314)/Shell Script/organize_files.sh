f3b4ade7e1708eab148db3eb04b8e6225a7b2e0b
tanjim17 <tanjim.ak49@gmail.com>
2021-12-04 21:14:31 +0600

    handle space

--- Operating System (CSE 314)/Shell Script/organize_files.sh
+++ Operating System (CSE 314)/Shell Script/organize_files.sh
@@ -3,12 +3,12 @@
 usage_msg="1st arg: working directory (optional. if not given, directory containing this script will be chosen)\n2nd arg: file containing list of types to be ignored\n"
 
 organize() {
-    if [ ! -d $1 ] || [ ! -f $2 ]; then
+    if [ ! -d "$1" ] || [ ! -f "$2" ]; then
         printf "no such file or directory!\n$usage_msg"
         return
     fi
 
-    sed 's/\r$//' $2 > temp_input.txt
+    sed 's/\r$//' "$2" > temp_input.txt
     readarray -t ignored_types < temp_input.txt
     
     output_dir="output_dir"
@@ -19,15 +19,15 @@ organize() {
     declare -A count
     count["ignored"]="0"
 
-    for path in $(find $1 -type f); do
-        filename=$(basename "$path")
-        extension=${filename##*.}
+    while read path; do
+        filename="$(basename "$path")"
+        extension="${filename##*.}"
 
         is_ignored="false"
         for type in "${ignored_types[@]}"; do
             if [ "$extension" = "$type" ]; then
                 is_ignored="true"
-                count["ignored"]=$((${count["ignored"]} + 1))
+                count["ignored"]="$(("${count["ignored"]}" + 1))"
                 break
             fi
         done
@@ -49,13 +49,13 @@ organize() {
         fi
         if [ ! -f "$dest/$filename" ]; then
             cp "$path" "$dest"
-            echo $path >> "$dest/desc_$subdir.txt"
-            count["$subdir"]=$((${count["$subdir"]} + 1))
+            echo "$path" >> "$dest/desc_$subdir.txt"
+            count["$subdir"]="$(("${count["$subdir"]}" + 1))"
         fi
-    done
+    done <<< "$(find "$1" -type f)"
 
     echo "file_type,no_of_files" > output.csv
-    for subdir in ${!count[@]}; do
+    for subdir in "${!count[@]}"; do
         echo "${subdir},${count["${subdir}"]}" >> output.csv
     done
 
@@ -63,9 +63,9 @@ organize() {
 }
 
 if [ $# -eq 1 ]; then
-    organize . $1
+    organize . "$1"
 elif [ $# -eq 2 ]; then
-    organize $1 $2
+    organize "$1" "$2"
 else
     printf "missing or too many arguments!\n$usage_msg"
 fi
