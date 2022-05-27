e2981f7650b61aa059b9d7131eef368a4a8149b4
Tanjim <tanjim.ak49@gmail.com>
2022-05-27 07:33:06 +0600

    formatting

--- Computer Security (CSE 406)/Cryptography/rsa.py
+++ Computer Security (CSE 406)/Cryptography/rsa.py
@@ -2,17 +2,6 @@ from ctypes.wintypes import CHAR
 from pydoc import pager
 from BitVector import *
 
-
-def input_plaintext():
-    file = open('input.txt', 'r')
-    plaintext = file.read()
-    file.close()
-    if len(plaintext) % 16 != 0:
-        plaintext = plaintext + ' ' * (16 - len(plaintext) % 16)
-    return plaintext
-    # return BitVector(textstring=plaintext)
-
-
 def generate_primes(k):
     bv = BitVector(intVal = 0)
     primes = set()
@@ -41,21 +30,3 @@ def generate_key_pair(k):
 
 def transform(char, key, n):
     return pow(char, key, n)
-
-# k = int(input('Enter value of k:'))
-k = 16
-# e_bv, d_bv, n_bv = generate_key_pair(k)
-e, d, n = generate_key_pair(k)
-# e, d, n = 45097, 2123962321, 2571216841
-
-# encryption
-plaintext = input_plaintext()
-cipher = []
-for p in plaintext:
-    cipher.append(transform(ord(p), e, n))
-
-# decrypt
-deciphered_text = ''
-for c in cipher:
-    deciphered_text += chr(transform(c, d, n))
-print(deciphered_text)
--- Computer Security (CSE 406)/Cryptography/rsa_demo.py
+++ Computer Security (CSE 406)/Cryptography/rsa_demo.py
@@ -1,5 +1,6 @@
 from rsa import *
 import time
+from prettytable import PrettyTable
 
 # input k
 k = 16
@@ -9,30 +10,38 @@ k = 16
 plaintext = 'CanTheyDoTheirFest'
 # plaintext = input('Enter plaintext: ')
 
-# key generation
-# e_bv, d_bv, n_bv = generate_key_pair(k)
-key_generation_time = time.time()
-e, d, n = generate_key_pair(k)
-key_generation_time = time.time() - key_generation_time
+time_info = PrettyTable(['K', 'Key Generation', 'Encryption', 'Decryption'])
 
-# encryption
-cipher = []
-encryption_time = time.time()
-for p in plaintext:
-    cipher.append(transform(ord(p), e, n))
-encryption_time = time.time() - encryption_time
+# time count
+for k in [16, 32, 64, 128]:
+    # key generation
+    # e_bv, d_bv, n_bv = generate_key_pair(k)
+    key_generation_time = time.perf_counter()
+    e, d, n = generate_key_pair(k)
+    key_generation_time = time.perf_counter() - key_generation_time
 
-# decryption
-deciphered_text = ''
-decryption_time = time.time()
-for c in cipher:
-    deciphered_text += chr(transform(c, d, n))
-decryption_time = time.time() - decryption_time
+    # encryption
+    cipher = []
+    encryption_time = time.perf_counter()
+    for p in plaintext:
+        cipher.append(transform(ord(p), e, n))
+    encryption_time = time.perf_counter() - encryption_time
 
-print('Generated Keys:\nPublic: ({}, {})\nPrivate: ({}, {})\n'.format(e, n, d, n))
-print('Plain Text:\n{}\n'.format(plaintext))
-print('Cipher:\n{}\n'.format(cipher))
-print('Deciphered Text:\n{}\n'.format(deciphered_text))
-print('Execution Time:\nKey Generation:', key_generation_time, 'seconds')
-print('Encryption:', encryption_time, 'seconds')
-print('Decryption:', decryption_time, 'seconds')
+    # decryption
+    deciphered_text = ''
+    decryption_time = time.perf_counter()
+    for c in cipher:
+        deciphered_text += chr(transform(c, d, n))
+    decryption_time = time.perf_counter() - decryption_time
+
+    print('k =', k)
+    print('Public Key: ({}, {})'.format(e, n))
+    print('Private Key: ({}, {})'.format(d, n))
+    print('Plain Text:', plaintext)
+    print('Cipher:', cipher)
+    print('Deciphered Text:', deciphered_text, '\n')
+    time_info.add_row([k, '{:.4e}'.format(key_generation_time), '{:.4e}'.format(encryption_time),
+                       '{:.4e}'.format(decryption_time)])
+
+print('Time Info (in seconds):')
+print(time_info)
