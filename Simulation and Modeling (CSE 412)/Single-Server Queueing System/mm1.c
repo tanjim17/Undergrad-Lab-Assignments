c568e934dbb4d47d90726d438a96f979ee142cb8
tanjim17 <tanjim.ak49@gmail.com>
2022-12-06 23:21:18 +0600

    minor change

--- Simulation and Modeling (CSE 412)/Single-Server Queueing System/mm1.c
+++ Simulation and Modeling (CSE 412)/Single-Server Queueing System/mm1.c
@@ -281,6 +281,6 @@ float expon(float mean)  /* Exponential variate generation function. */
 {
     /* Return an exponential random variate with mean "mean". */
 
-    return -mean * log(lcgrand(1.5));
+    return -mean * log(lcgrand(1));
 }
 
